3ab8c4b89212debe5377ca4ac85fbe6ca3ca94c1
Nicholas Wise <nicholasmwise@gmail.com>
2025-03-10 14:20:25 -0500

    Implemented basic methods for linked list, and started work on the Vector Stack

--- Single_Linked_List.h
+++ Single_Linked_List.h
@@ -15,8 +15,13 @@ public:
     Item_Type* tail{};
     int num_items = 0;
 private:
-    void push_front();
-    void push_back();
+    struct Node {
+        Item_Type* data;
+        Node* next;
+        Node(Item_Type* value) : data(value), next(nullptr) {};
+    };
+    void push_front(Item_Type value);
+    void push_back(Item_Type value);
     void pop_front();
     void pop_back();
     Item_Type front();
@@ -28,37 +33,76 @@ private:
 
 template<typename Item_Type>
 Single_Linked_List<Item_Type>::Single_Linked_List() {
-
+    head = nullptr;
+    tail = nullptr;
 }
 
 template<typename Item_Type>
-void Single_Linked_List<Item_Type>::push_front() {
-
+void Single_Linked_List<Item_Type>::push_front(Item_Type value) {
+    if (head == nullptr) {
+        head = new Node(value);
+        tail = head;
+        num_items++;
+        return;
+    }
+    Node* new_node = new Node(value);
+    new_node->next = head;
+    head = new_node;
+    num_items++;
 }
 
 template<typename Item_Type>
-void Single_Linked_List<Item_Type>::push_back() {
-
+void Single_Linked_List<Item_Type>::push_back(Item_Type value) {
+    if (head == nullptr) {
+        head = new Node(value);
+        tail = head;
+        num_items++;
+        return;
+    }
+    Node* new_node = new Node(value);
+    tail->next = new_node;
+    tail = new_node;
+    num_items++;
 }
 
 template<typename Item_Type>
 void Single_Linked_List<Item_Type>::pop_front() {
-
+    if (head == nullptr) {
+        return;
+    }
+    if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
+        num_items--;
+        return;
+    }
+    delete head;
+    head = head->next;
+    num_items--;
 }
 
 template<typename Item_Type>
 void Single_Linked_List<Item_Type>::pop_back() {
-
+    if (tail == nullptr) {
+        return;
+    }
+    if (head->next == nullptr) {
+        delete head;
+        delete tail;
+        head = nullptr;
+        tail = nullptr;
+        num_items--;
+    }
 }
 
 template<typename Item_Type>
 Item_Type Single_Linked_List<Item_Type>::front() {
-
+    return head->data;
 }
 
 template<typename Item_Type>
 Item_Type Single_Linked_List<Item_Type>::back() {
-
+    return tail->data;
 }
 
 template<typename Item_Type>
@@ -75,7 +119,24 @@ template<typename Item_Type>
 size_t Single_Linked_List<Item_Type>::find(const Item_Type &item) {
 
 }
-
-
+struct Node {
+    int data;
+    Node* next;
+    Node(int value) : data(value), next(nullptr) {}
+};
+class Stack{
+public:
+    Stack();
+    ~Stack();
+    Node* head{};
+    Node* tail{};
+    int num_items = 0;
+private:
+    bool empty() const;
+    void insert(int value);
+    int remove();
+    int top();
+    float average();
+};
 
 #endif //SINGLE_LINKED_LIST_H
