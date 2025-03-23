c7855fa82f4a54e3c0e605abc704b4c305ef959a
Nicholas Wise <nicholasmwise@gmail.com>
2025-03-22 22:07:41 -0500

    Updated comments

--- Single_Linked_List.h
+++ Single_Linked_List.h
@@ -7,6 +7,23 @@
 using namespace std;
 #include <vector>
 #include <iostream>
+// Linked List Class
+// Any data type
+// push_front
+// push_back
+// pop_front
+// pop_back
+// front, back
+// empty
+
+// void insert(size_t index, const Item_Type& item): Insert item at position index
+// (starting at 0). Insert at the end if index is beyond the end of the list
+
+// bool remove(size_t index): Remove the item at position index. Return true if
+// successful; return false if index is beyond the end of the list.
+
+// size_t find(const Item_Type& item): Return the position of the first occurrence of
+// item if it is found. Return the size of the list if it is not found.
 
 template <typename Item_Type>
 class Single_Linked_List {
